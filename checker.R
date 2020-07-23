68f799daa80d5650a9dddb4082abd73be37152bf
Abitama Satria <56988153+abitamaspp@users.noreply.github.com>
2020-07-23 14:58:59 +0700

    Add comments on line 7 and15

--- checker.R
+++ checker.R
@@ -4,6 +4,7 @@ A <- gsub("[[:punct:]]", "", A)
 A <- gsub("\\s", "", A)
 j <-  nchar(A)
 
+#Loop over half of string's length
 for (i in 1:floor(nchar(A)/2)){
   if(substr(A,i,i) != substr(A,j,j)){
     break()
@@ -11,6 +12,7 @@ for (i in 1:floor(nchar(A)/2)){
   j <- nchar(A)-i
 }
 
+#Check result of j
 if(j == floor(nchar(A)/2) + 1 | j == i){ 
   print("Palindrome cuy")
 } else {
